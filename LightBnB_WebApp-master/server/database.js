e13def5ce1a0d38e13bfeba6a8ebd7a2432caf3c
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-10-16 02:54:03 +0000

    Updated add properties function to insert user input data to create a new property listing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -182,7 +182,7 @@ ORDER BY cost_per_night
 LIMIT $${queryParams.length};
 `;
 
-console.log(queryString, queryParams);
+//console.log(queryString, queryParams);
 
 return pool.query(queryString, queryParams)
   .then(res => res.rows);
@@ -209,9 +209,36 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `
+  const values = [property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bedrooms,
+    property.number_of_bathrooms]
+
+    return pool.query(queryString, values)
+    .then (res => {
+      return res.rows[0]
+    })
+    .catch(err => console.error('query error', err.stack));
+
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
